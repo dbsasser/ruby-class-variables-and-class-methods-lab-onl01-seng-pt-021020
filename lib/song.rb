432ce891898151bc778456841a961455bc76cf96
Daniel Sasser <danielbsasser@gmail.com>
2020-02-16 18:02:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,16 @@ class Song
     @@count
   end
   
-  def genres 
-    @@genres
+  def genres
+    genre_list = []
+    @@genres.each do |genre|
+      if genre_list.include?(genre)
+        
+      else 
+        genre_list << genre
+      end
+      
+    genre_list
   end
   
 end
